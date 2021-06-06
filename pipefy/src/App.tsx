21b0804eaaba0836a255bb796ccf9f7fd901ccfd
Allan Carlos <allancarlosdeveloper@gmail.com>
2021-06-06 00:07:20 -0300

    prepare pipefy project

--- pipefy/src/App.tsx
+++ pipefy/src/App.tsx
@@ -1,12 +1,6 @@
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </header>
-    </div>
+    <div className="App"></div>
   );
 }
 
